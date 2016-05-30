43098e20bdfb2be2d0dcc4bc35f8134b6fb8a770
mama <iatmotherprogrammer@gmail.com>
2016-05-30 16:41:04 +0500

    add parse thread

--- Wpf2chdownloader/Wpf2chdownloader/MainWindow.xaml.cs
+++ Wpf2chdownloader/Wpf2chdownloader/MainWindow.xaml.cs
@@ -1,6 +1,8 @@
-﻿using System;
+﻿using Newtonsoft.Json;
+using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Net;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows;
@@ -12,6 +14,7 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
+using Wpf2chdownloader.Models;
 
 namespace Wpf2chdownloader
 {
@@ -25,9 +28,66 @@ namespace Wpf2chdownloader
             InitializeComponent();
         }
 
+    
+        public Rootobject loadThread(Uri url)
+        {
+            var thread = new Rootobject();
+            try
+            {
+                string s = url.AbsoluteUri;
+                var json = new WebClient { Encoding = Encoding.UTF8 }.DownloadString(url.AbsoluteUri.Substring(0, s.LastIndexOf(".")) + ".json");
+                thread = JsonConvert.DeserializeObject<Rootobject>(json);
+            }
+            catch
+            {
+            }
+            return thread; 
+        }
+
+        
+
+        public List<File> parseThread(Rootobject thread)
+        {
+            //var test = thread.threads[0].posts;
+            var fileList = new List<File>();
+            foreach (var item in thread.threads[0].posts)
+            {
+                if (item.files.Count() > 0)
+                    foreach (var file in item.files)
+                    {
+                        file.path = "https://2ch.hk/"  + thread.Board +"/"+ file.path;
+                        fileList.Add(file);
+                    }   
+            }
+            return fileList;
+       }
+
+
         private void loadButton_Click(object sender, RoutedEventArgs e)
         {
-            
+            Uri url;
+            var thread = new Rootobject();
+            try
+            {
+                url = new Uri(InputUrlBox.Text);
+                thread = loadThread(url);
+                var fileLIst  = parseThread(thread);
+                typeFileComboBox.Items.Clear();
+                typeFileComboBox.Items.Add("Все");
+                typeFileComboBox.SelectedItem = "Все";
+                var typeList = new List<string>();
+                foreach (var item in fileLIst)
+                {
+                    if (!typeFileComboBox.Items.Contains(item.type + " - " + item.name.Substring(item.name.IndexOf("."))))
+                    {
+                       // typeList.Add(item.type);
+                        typeFileComboBox.Items.Add(item.type +" - " + item.name.Substring(item.name.IndexOf(".")));
+                    }   
+                }
+            }
+            catch
+            {
+            }
         }
     }
 }
